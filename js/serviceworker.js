fb4a9e3385201d9e528e6e820e555330efe2e44d
MillCreeker <59824109+MillCreeker@users.noreply.github.com>
2020-11-25 18:53:07 +0100

    cleanup

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -24,7 +24,7 @@ var filesToCache = [
 ];
 
 self.addEventListener("install", event => {
-  console.log("Service Worker Lab 04 PWA installing.");
+  console.log("Service Worker Cryptolatr installing.");
   event.waitUntil(
       caches.open(cacheName).then(function(cache) {
         return cache.addAll(filesToCache);
@@ -33,11 +33,11 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("activate", event => {
-  console.log("Service Worker Lab 04 PWA activating.");
+  console.log("Service Worker Cryptolatr activating.");
 });
 
 self.addEventListener('fetch', event => {
-  console.log("Service Worker Lab 04 PWA fetching.");
+  console.log("Service Worker Cryptolatr fetching.");
   event.respondWith(
       caches.match(e.request).then(function(response) {
         return response || fetch(e.request);
