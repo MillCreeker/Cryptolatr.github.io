3679a70d8a20836ad3f92ba012b2b2ebb5441bf3
MillCreeker <59824109+MillCreeker@users.noreply.github.com>
2020-11-25 17:39:16 +0100

    serviceworker fix

--- serviceworker.js
+++ serviceworker.js
@@ -3,24 +3,24 @@
  ******************/
 var cacheName = 'lab04pwa-cache';
 var filesToCache = [
-  '../img/apple_splash_640.png',
-  '../img/apple_splash_750.png',
-  '../img/apple_splash_1125.png',
-  '../img/apple_splash_1242.png',
-  '../img/apple_splash_1536.png',
-  '../img/apple_splash_1668.png',
-  '../img/apple_splash_2048.png',
-  '../img/touch-icon-iphone.png',
-  '../img/lab04pwa48.png',
-  '../img/lab04pwa72.png',
-  '../img/lab04pwa96.png',
-  '../img/lab04pwa144.png',
-  '../index.html',
-  '../css/style.css',
-  'use_webworker.js',
-  'use_serviceworker.js',
-  'webworker.js',
-  'main.js'
+  './img/apple_splash_640.png',
+  './img/apple_splash_750.png',
+  './img/apple_splash_1125.png',
+  './img/apple_splash_1242.png',
+  './img/apple_splash_1536.png',
+  './img/apple_splash_1668.png',
+  './img/apple_splash_2048.png',
+  './img/touch-icon-iphone.png',
+  './img/lab04pwa48.png',
+  './img/lab04pwa72.png',
+  './img/lab04pwa96.png',
+  './img/lab04pwa144.png',
+  './index.html',
+  './css/style.css',
+  './js/use_webworker.js',
+  './js/use_serviceworker.js',
+  './js/webworker.js',
+  './js/main.js'
 ];
 
 self.addEventListener("install", event => {
