3e352d2ad277ffcdc35cad6272c1317d068781a3
MillCreeker <59824109+MillCreeker@users.noreply.github.com>
2020-12-01 09:37:21 +0100

    made it prettier

--- js/menu_bar.js
+++ js/menu_bar.js
@@ -1,12 +1,14 @@
 var apple = "Apple";
 
 function menu_open(){
+    document.body.style.backgroundColor = "#C4C4C4";
     document.getElementById("menu_sidebar").style.display = "block";
     var content = document.getElementsByClassName("content");
     content[0].style.pointerEvents = "none";
 }
 
 function menu_close(){
+    document.body.style.backgroundColor = "#FFFFFF";
     document.getElementById("menu_sidebar").style.display = "none";
     var content = document.getElementsByClassName("content");
     content[0].style.pointerEvents = "all";
