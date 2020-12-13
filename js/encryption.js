3b75b27038e37cd0a30b1addbf84b30ac99a9489
Anna-Caterina Amann <anna-caterina.amann@edu.fh-joanneum.at>
2020-12-13 05:53:47 +0100

    completed encryption page

--- js/encryption.js
+++ js/encryption.js
@@ -10,6 +10,19 @@ function save_seed_encrypt(){
     localStorage.setItem("seedKey", seed);
     window.open("encryption.html","_self");
 }
+function goBack() {
+  window.history.back();
+}
+function encryptcopy(message = '', key = ''){
+    var message = CryptoJS.AES.encrypt(message, key);
+    var messStr = message.toString();
+    document.getElementById('textEncr').value = messStr;
+    var copyText = document.getElementById("textEncr");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    return messStr;
+}
 
 // Um mit seed arbeiten zu können, einfach folgende Zeile am Anfang der ersten Funktion einbauen:
 // seed = localStorage.getItem("seedKey");
